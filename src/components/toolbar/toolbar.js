3e5486b4376ecadd3ed4523d87af7895db4c4033
Moritz Riede <moritz@mriede.com>
2025-07-13 19:10:25 +0200

    feat: add import style button and rename data loading button

--- src/components/toolbar/toolbar.js
+++ src/components/toolbar/toolbar.js
@@ -129,7 +129,11 @@ export class StyleEditorToolbar extends LitElement {
             </div>
             <button @click="${this._triggerFileLoad}" class="right-round">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>import</title><path d="M14,12L10,8V11H2V13H10V16M20,18V6C20,4.89 19.1,4 18,4H6A2,2 0 0,0 4,6V9H6V6H18V18H6V15H4V18A2,2 0 0,0 6,20H18A2,2 0 0,0 20,18Z" /></svg>
-              <span>Load</span>
+              <span>Open</span>
+            </button>
+            <button @click="${() => {}}" style="margin-left: 9px;">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>palette-swatch</title><path d="M2.53,19.65L3.87,20.21V11.18L1.44,17.04C1.03,18.06 1.5,19.23 2.53,19.65M22.03,15.95L17.07,4C16.76,3.23 16.03,2.77 15.26,2.75C15,2.75 14.73,2.79 14.47,2.9L7.1,5.95C6.35,6.26 5.89,7 5.87,7.75C5.86,8 5.91,8.29 6,8.55L11,20.5C11.29,21.28 12.03,21.74 12.81,21.75C13.07,21.75 13.33,21.7 13.58,21.6L20.94,18.55C21.96,18.13 22.45,16.96 22.03,15.95M7.88,8.75A1,1 0 0,1 6.88,7.75A1,1 0 0,1 7.88,6.75C8.43,6.75 8.88,7.2 8.88,7.75C8.88,8.3 8.43,8.75 7.88,8.75M5.88,19.75A2,2 0 0,0 7.88,21.75H9.33L5.88,13.41V19.75Z" /></svg>
+              <span>Import style</span>
             </button>
           </nav>
 
